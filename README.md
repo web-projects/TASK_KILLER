1722c0edaa2904c1502df59e90e9d5ae5df4c256
Jon <jon.bianco@trustcommerce.com>
2020-04-08 20:29:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# README #
+
+This is an application to display and kill task families.
+
+### What is this repository for? ###
+
+* TASK HANDLER APPLICATION
+* 1.00.1.(100)
+* git remote add origin https://github.com/web-projects/TASK_KILLER.git
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### GIT NOTES ###
+
+*  AUTO-CONVERTING CRLF line endings into LF
+   $ git config --global core.autocrlf true
+   
+### HISTORY ###
+
+* 20191218 - Initial repository
+           - Added WaitAsync implementation
+* 20191219 - Enhanced Async methods
+* 20200122 - Cleaned up CancellationToken
+* 20200123 - Improved Cancellation workflow
+* 20200206 - Cleaned up README file
---
+++ TASK_KILLER/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace TASK_KILLER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            programBox.SelectAll();
+        }
+
+        private void OnAppTextChanged(object sender, EventArgs e)
+            => getAppBtn.Enabled = (programBox.Text.Length > 0) ? true : false;
+
+        private void SetDefaults()
+        {
+            new Thread(async () =>
+            {
+                await Task.Delay(3000);
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    messagelbl.Text = "WAITING FOR COMMAND...";
+                }));
+            }).Start();
+        }
+
+        private void OnItemChecked(object sender, ItemCheckEventArgs e)
+        {
+            new Thread(() =>
+            {
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    deleteAppBtn.Enabled = AppCheckBoxList.CheckedItems.Count > 0 ? true : false;
+                }));
+            }).Start();
+        }
+
+        private void getAppBtn_Click(object sender, EventArgs e)
+        {
+            deleteAppBtn.Enabled = false;
+            AppCheckBoxList.Items.Clear();
+
+            Process[] processList = Process.GetProcesses();
+
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName.StartsWith(programBox.Text))
+                {
+                    AppCheckBoxList.Items.Add(process.ProcessName);
+                }
+            }
+            // Sort Items
+            var items = AppCheckBoxList.Items
+                .Cast<string>()
+                .OrderBy(i => i)
+                .ToArray();
+            AppCheckBoxList.Items.Clear();
+            AppCheckBoxList.Items.AddRange(items);
+
+            // Set default items
+            SetDefaults();
+        }
+
+        private void deleteAppBtn_Click(object sender, EventArgs e)
+        {
+            getAppBtn.Enabled = false;
+            deleteAppBtn.Enabled = false;
+            AppCheckBoxList.Enabled = false;
+
+            foreach (string appName in AppCheckBoxList.CheckedItems)
+            {
+                messagelbl.Text = $"{appName} process is stopped.";
+
+                Process[] ps = Process.GetProcessesByName(appName);
+
+                foreach (Process p in ps)
+                {
+                    p.Kill();
+                }
+            }
+            AppCheckBoxList.Items.Clear();
+
+            new Thread(async () =>
+            {
+                await Task.Delay(5000);
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    getAppBtn_Click(sender, e);
+                    AppCheckBoxList.Enabled = true;
+                    getAppBtn.Enabled = true;
+                }));
+            }).Start();
+        }
+    }
+}
